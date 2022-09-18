ab58f83fce086878f48d7f3934b6b788930d8775
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 17:58:22 +0900

    섹션3-8, 9

--- 파이썬-알고리즘-문제풀이/섹션 3/8. 곳감/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 3/8. 곳감/AA.py
@@ -2,27 +2,30 @@ import sys
 #sys.stdin=open("input.txt", "r")
 
 N = int(input())
-gotgams = [list(map(int, input().split())) for _ in range(N)]
-M  = int(input())
-orders = [list(map(int, input().split())) for _ in range(M)]
+arr = [list(map(int, input().split())) for _ in range(N)]
+M = int(input())
+order = [list(map(int, input().split())) for _ in range(M)]
 
 for i in range(M):
-  a, b, c = orders[i]
+  a, b, c = order[i]
+
   if b == 0:
-    for j in range(c):
-      gotgams[a-1].append(gotgams[a-1].pop(0))
+    for _ in range(c):
+      arr[a-1].append(arr[a-1].pop(0))
   else:
-    for j in range(c):
-      gotgams[a-1].insert(0, gotgams[a-1].pop(N-1))
-s = 0
-e = N - 1
+    for _ in range(c):
+      arr[a-1].insert(0, arr[a-1].pop())
+
+s = 0 
+e = N - 1 
 summ = 0
 for i in range(N):
-  summ += sum(gotgams[i][s:e+1])
-  if i < N // 2:
+  summ += sum(arr[i][s:e+1])
+  if i < N//2:
     s += 1
     e -= 1
   else:
     s -= 1
     e += 1
 print(summ)
+
--- 파이썬-알고리즘-문제풀이/섹션 3/9. 봉우리/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 3/9. 봉우리/AA.py
@@ -1,22 +1,19 @@
 import sys
 #sys.stdin=open("input.txt", "r")
-
 dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+dy = [0, 1, 0, -1]
+
 N = int(input())
 bongs = [list(map(int, input().split())) for _ in range(N)]
-
-bongs.append([0] * N)
-bongs.insert(0, [0] * N)
+bongs.insert(0, [0]*N)
+bongs.append([0]*N)
 
 for i in range(N+2):
   bongs[i].insert(0, 0)
   bongs[i].append(0)
-
 cnt = 0
-
 for i in range(1, N+1):
   for j in range(1, N+1):
-    if all(bongs[i][j] >  bongs[i+dx[k]][j+dy[k]] for k in range(4) ):
+    if all(bongs[i][j] > bongs[i+dx[k]][j+dy[k]] for k in range(4)):
       cnt += 1
 print(cnt)
