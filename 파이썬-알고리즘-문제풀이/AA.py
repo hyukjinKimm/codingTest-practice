37d5bcc5c6ea88f955cbefb6409474f2a5f8f09f
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 23:18:07 +0900

    섹션3-5

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -1,31 +1,16 @@
 import sys
 sys.stdin=open("input.txt", "r")
 
-
-
-def Count(dist): # dist 이상으로 말을 배치했을때 배치가능한 말의 수 
-  cnt = 1 
-  p = point[0]
-  for i in range(1, N):
-    if point[i] - p >= dist:
-      cnt += 1
-      p = point[i]
-  return cnt
-
-N, C = map(int, input().split())
-point = []
+N =int(input())
+time = []
 for i in range(N):
-  point.append(int(input()))
-point.sort()
+  time.append(list(map(int, input().split())))
 
-lt = 1 
-rt = point[N-1] -  point[0]
-while lt <= rt:
-  mid = (lt + rt) // 2
-  if Count(mid) >= C:
-    res = mid 
-    lt = mid + 1 
-  else:
-    rt = mid - 1
-
-print(res)
+time.sort(key=lambda x: x[1])
+endtime = - 1
+cnt = 0
+for i in range(N):
+  if time[i][0] >= endtime:
+    cnt += 1
+    endtime = time[i][1]
+print(cnt)
--- 파이썬-알고리즘-문제풀이/섹션 4/5. 회의실 배정/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 4/5. 회의실 배정/AA.py
@@ -1,18 +1,16 @@
 import sys
 #sys.stdin=open("input.txt", "r")
 
-N = int(input())
+N =int(input())
 time = []
 for i in range(N):
-  s, e =map( int, input().split())
-  time.append([s, e])
-time.sort(key=lambda x:x[1])
+  time.append(list(map(int, input().split())))
 
-
+time.sort(key=lambda x: x[1])
 endtime = - 1
-cnt = 0 
-for t in time:
-  if t[0] >= endtime:
-    endtime = t[1]
+cnt = 0
+for i in range(N):
+  if time[i][0] >= endtime:
     cnt += 1
+    endtime = time[i][1]
 print(cnt)
