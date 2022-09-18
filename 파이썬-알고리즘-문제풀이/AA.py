192f1b5944b5cd1be9765de609a4dc93e2257f62
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 20:33:11 +0900

    섹션3-2

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -1,18 +1,27 @@
 import sys
 sys.stdin=open("input.txt", "r")
 
-N, M = map(int, input().split())
-arr = list(map(int, input().split()))
-arr.sort()
-lt = 0
-rt = N - 1
+def Count(x): # x만큼 잘랐을때 나오는 라인의 갯수 
+  cnt = 0 
+  for l in line:
+    cnt += l // x 
+  return cnt
 
+
+K, N = map(int, input().split())
+line = []
+for i in range(K):
+  line.append(int(input()))
+line.sort()
+lt = 1 
+rt = line[K-1]
+
+res = -1
 while lt <= rt:
   mid = (lt + rt) // 2
-  if arr[mid] == M:
-    print(mid+1)
-    break
-  elif arr[mid] > M:
-    rt = mid - 1
+  if Count(mid) >= N :
+    res = mid
+    lt = mid +1 
   else:
-    lt = mid + 1
+    rt = mid - 1
+print(res)
--- 파이썬-알고리즘-문제풀이/섹션 4/2. 랜선자르기/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 4/2. 랜선자르기/AA.py
@@ -1,25 +1,27 @@
 import sys
 #sys.stdin=open("input.txt", "r")
 
-def Count(x): # x만큼 으로 잘랐을때 몇개의 라인이 나오나? 
-  cnt = 0
+def Count(x): # x만큼 잘랐을때 나오는 라인의 갯수 
+  cnt = 0 
   for l in line:
     cnt += l // x 
   return cnt
+
+
 K, N = map(int, input().split())
 line = []
 for i in range(K):
   line.append(int(input()))
+line.sort()
+lt = 1 
+rt = line[K-1]
 
-
-lt = 1
-rt = max(line)
 res = -1
 while lt <= rt:
   mid = (lt + rt) // 2
-  if Count(mid) >= N:
-    res = mid 
-    lt = mid + 1
+  if Count(mid) >= N :
+    res = mid
+    lt = mid +1 
   else:
     rt = mid - 1
 print(res)
