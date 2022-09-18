968ccb95270971c347fd14f16b8e6a7f9f526e60
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 13:45:03 +0900

    섹션2-7

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -1,18 +1,15 @@
 import sys
-#sys.stdin=open("input.txt", "r")
+sys.stdin=open("input.txt", "r")
 
-def digit_sum(x):
-  res = 0
-  while x > 0:
-    res += x % 10
-    x /= 10
-  return res
 
 N = int(input())
-number = list(map(int ,input().split()))
-maxx = -1
-res = -1
-for i in range(N):
-  if digit_sum(number[i]) > maxx:
-    res = i 
-print(number[res])
+ch = [0] * (N+1)
+cnt = 0
+for i in range(2, N+1):
+  if ch[i] == 0:
+    cnt += 1
+    for j in range(i, N+1, i):
+      ch[j] = 1
+print(cnt)
+
+
