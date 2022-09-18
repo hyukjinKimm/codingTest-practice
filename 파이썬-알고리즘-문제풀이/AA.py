eed3d93fa6eb42a8f6f03c250e054d6e494277c1
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 18:06:16 +0900

    섹션3-10

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -1,19 +1,29 @@
 import sys
 sys.stdin=open("input.txt", "r")
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
 
-N = int(input())
-bongs = [list(map(int, input().split())) for _ in range(N)]
-bongs.insert(0, [0]*N)
-bongs.append([0]*N)
 
-for i in range(N+2):
-  bongs[i].insert(0, 0)
-  bongs[i].append(0)
-cnt = 0
-for i in range(1, N+1):
-  for j in range(1, N+1):
-    if all(bongs[i][j] > bongs[i+dx[k]][j+dy[k]] for k in range(4)):
-      cnt += 1
-print(cnt)
+
+Sudoku = [list(map(int, input().split())) for _ in range(9)]
+
+for i in range(9):
+  ch1 = [0] * 10
+  ch2 = [0] * 10
+  for j in range(9):
+    ch1[Sudoku[i][j]] = 1
+    ch2[Sudoku[j][i]] = 1
+  if sum(ch1) != 9 or sum(ch2) != 9:
+    print('NO')
+    sys.exit()
+else:
+  for i in range(0, 9, 3):
+    for j in range(0, 9, 3):
+      ch1 = [0] * 10
+      for k in range(3):
+        for n in range(3):
+          ch1[Sudoku[i+k][j+n]] = 1 
+      if sum(ch1) != 9:
+        print('NO')
+        sys.exit()
+  else:
+    print('YES')
+
--- 파이썬-알고리즘-문제풀이/섹션 3/10. 스도쿠 검사/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 3/10. 스도쿠 검사/AA.py
@@ -1,26 +1,29 @@
 import sys
 #sys.stdin=open("input.txt", "r")
 
-sdoku = [list(map(int, input().split())) for _ in range(9)]
+
+
+Sudoku = [list(map(int, input().split())) for _ in range(9)]
 
 for i in range(9):
-  ch1 =[0] * 10
-  ch2 =[0] * 10
+  ch1 = [0] * 10
+  ch2 = [0] * 10
   for j in range(9):
-    ch1[sdoku[i][j]] += 1
-    ch2[sdoku[j][i]] += 1
-  if ch1.count(0) != 1 or ch2.count(0) != 1:
+    ch1[Sudoku[i][j]] = 1
+    ch2[Sudoku[j][i]] = 1
+  if sum(ch1) != 9 or sum(ch2) != 9:
     print('NO')
-    break
+    sys.exit()
 else:
   for i in range(0, 9, 3):
     for j in range(0, 9, 3):
-      ch1 =[0] * 10
+      ch1 = [0] * 10
       for k in range(3):
-        for m in range(3):
-          ch1[sdoku[i+k][j+m]] += 1
-    if ch1.count(0) != 1:
-      print('NO')
-      break
+        for n in range(3):
+          ch1[Sudoku[i+k][j+n]] = 1 
+      if sum(ch1) != 9:
+        print('NO')
+        sys.exit()
   else:
     print('YES')
+
