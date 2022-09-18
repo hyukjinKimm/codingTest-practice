daa6be14f77be77b923b9b2ea84fce0760f7eaef
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 14:23:21 +0900

    섹션3-1

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -2,16 +2,13 @@ import sys
 sys.stdin=open("input.txt", "r")
 
 N = int(input())
-score = list(map(int, input().split()))
-
-before = 0 
-res = 0
-
 for i in range(N):
-  if score[i] == 1:
-   
-    before += 1
-    res += before
+  word = input()
+  word = word.upper()
+  for j in range(len(word)//2):
+    if word[j] != word[-1-j]:
+      print('#%d NO' %(i+1))
+      break
   else:
-    before = 0 
-print(res)
+      print('#%d YES' %(i+1)) 
+
--- 파이썬-알고리즘-문제풀이/섹션 3/1. 회문 문자열 검사/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 3/1. 회문 문자열 검사/AA.py
@@ -1,12 +1,14 @@
 import sys
 #sys.stdin=open("input.txt", "r")
 
-
 N = int(input())
 for i in range(N):
   word = input()
   word = word.upper()
-  if word == word[::-1]:
-    print("#%d YES" %(i+1))
+  for j in range(len(word)//2):
+    if word[j] != word[-1-j]:
+      print('#%d NO' %(i+1))
+      break
   else:
-    print("#%d NO" %(i+1))  
+      print('#%d YES' %(i+1)) 
+
