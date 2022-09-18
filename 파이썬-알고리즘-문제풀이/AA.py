1b584a91ba30bcc496184eb629b73eb7cebf2089
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 20:11:55 +0900

    섹션3-10

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -3,27 +3,19 @@ sys.stdin=open("input.txt", "r")
 
 
 
-Sudoku = [list(map(int, input().split())) for _ in range(9)]
+board = [list(map(int, input().split())) for _ in range(7)]
 
-for i in range(9):
-  ch1 = [0] * 10
-  ch2 = [0] * 10
-  for j in range(9):
-    ch1[Sudoku[i][j]] = 1
-    ch2[Sudoku[j][i]] = 1
-  if sum(ch1) != 9 or sum(ch2) != 9:
-    print('NO')
-    sys.exit()
-else:
-  for i in range(0, 9, 3):
-    for j in range(0, 9, 3):
-      ch1 = [0] * 10
-      for k in range(3):
-        for n in range(3):
-          ch1[Sudoku[i+k][j+n]] = 1 
-      if sum(ch1) != 9:
-        print('NO')
-        sys.exit()
-  else:
-    print('YES')
+cnt = 0
+for i in range(7):
+  for j in range(3):
+    tmp = board[i][j:j+5]
+    if tmp == tmp[::-1]:
+      cnt += 1
 
+  for j in range(3):
+    for k in range(2):
+      if board[j+k][i] != board[4+j-k][i]:
+        break 
+    else:
+      cnt += 1 
+print(cnt)
--- 파이썬-알고리즘-문제풀이/섹션 3/11. 격자판 회문수/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 3/11. 격자판 회문수/AA.py
@@ -1,23 +1,21 @@
 import sys
 #sys.stdin=open("input.txt", "r")
 
+
+
 board = [list(map(int, input().split())) for _ in range(7)]
 
 cnt = 0
-
-
 for i in range(7):
   for j in range(3):
     tmp = board[i][j:j+5]
-    if board[i][j:j+5] == tmp[::-1]:
-      cnt +=1
-
-    for k in range(2):
-      if board[j+k][i] != board[5+j-k-1][i]:
-        break
-    else:
+    if tmp == tmp[::-1]:
       cnt += 1
-      
-print(cnt)
-  
 
+  for j in range(3):
+    for k in range(2):
+      if board[j+k][i] != board[4+j-k][i]:
+        break 
+    else:
+      cnt += 1 
+print(cnt)
