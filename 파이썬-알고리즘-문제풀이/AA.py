b1dc7a492d5f65c5c09511b5ab02d8b0f8059036
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-18 13:12:20 +0900

    섹션2-1

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -1,19 +1,15 @@
 import sys
 sys.stdin=open("input.txt", "r")
-N = int(input())
-arr = list(map(int ,input().split()))
-result = [0] * (N+1)
+N, K = list(map(int, input().split()))
 
-for i in range(N):
-  z = 0
-  for j  in range(1, N + 1):
-    if z == arr[i] and result[j] == 0:
-      result[j] = i+1
-      break
-    if result[j] == 0:
-      z += 1
-for i in range(1, N+1):
-  print(result[i], end = ' ')
+cnt = 0
 
-  
+for i in range(1, N + 1):
+  if N % i == 0:
+    cnt += 1 
+  if cnt == K:
+    print(i)
+    break 
+else:
+  print(-1)
     
--- 파이썬-알고리즘-문제풀이/섹션 2/1. k번째 약수/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 2/1. k번째 약수/AA.py
@@ -1,13 +1,15 @@
 import sys
 #sys.stdin=open("input.txt", "r")
+N, K = list(map(int, input().split()))
 
-n, k = map(int, input().split())
 cnt = 0
-for i in range(1, n+1):
-  if n % i == 0:
-    cnt += 1
-  if cnt == k:
+
+for i in range(1, N + 1):
+  if N % i == 0:
+    cnt += 1 
+  if cnt == K:
     print(i)
     break 
 else:
-    print(-1)
+  print(-1)
+    
