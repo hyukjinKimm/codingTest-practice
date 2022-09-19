a31d0eca77dfc7ccaf61167c4372f0e6ed5b543b
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-19 12:46:06 +0900

    섹션6-3

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -2,11 +2,23 @@ import sys
 
 sys.stdin=open("input.txt", "r")
 
+
 def dfs(x):
-  if x == 0:
-    return
+  if x > N:
+    for i in range(1, N+1):
+      if ch[i] == 1:
+        print(i, end = ' ')
+    print()
   else:
-    dfs(x // 2)
-    print(x%2, end = '')
-n = int(input())
-dfs(n)
+    ch[x] = 1
+    dfs(x+1)
+    ch[x] = 0 
+    dfs(x+1)
+
+
+N = int(input())
+ch = [0] * (N+1)
+dfs(1)
+
+
+
