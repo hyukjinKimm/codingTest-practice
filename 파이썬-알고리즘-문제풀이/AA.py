29fc4695bc68388b6fbd476e7db505c7acbbd186
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-28 12:44:45 +0900

    섹션5-1

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -2,29 +2,19 @@ import sys
 
 sys.stdin=open("input.txt", "r")
 
-
-def dfs(L):
-  global cnt
-  if L == M:
-    for x in res:
-      print(x, end = ' ')
-    print()
-    cnt += 1
-  else:
-    for i in range(1, N+1):
-      if ch[i] == 0:
-        ch[i] = 1
-        res[L] = i
-        dfs(L+1)
-        ch[i] = 0
-    
-
-
 N, M = map(int, input().split())
-arr = [i+1 for i in range(N)]
-ch = [0] * (N+1)
-res = [0]*M
-cnt = 0
-dfs(0)
-print(cnt)
+N = list(map(int, str(N)))
 
+stack = []
+
+for i in range(len(N)):
+  while stack and stack[-1] < N[i] and M > 0:
+    stack.pop()
+    M -= 1
+  stack.append(N[i])
+if M > 0:
+  for x in stack[:-M]:
+    print(x, end = '')
+else:
+  for x in stack:
+    print(x, end = '')
--- 파이썬-알고리즘-문제풀이/섹션 5/1. 가장 큰 수/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 5/1. 가장 큰 수/AA.py
@@ -1,18 +1,20 @@
 import sys
+
 #sys.stdin=open("input.txt", "r")
 
 N, M = map(int, input().split())
 N = list(map(int, str(N)))
+
 stack = []
-for n in N:
-  while stack and stack[-1] < n and M > 0:
+
+for i in range(len(N)):
+  while stack and stack[-1] < N[i] and M > 0:
     stack.pop()
     M -= 1
-  stack.append(n)
-
-if M == 0:
-  for x in stack:
+  stack.append(N[i])
+if M > 0:
+  for x in stack[:-M]:
     print(x, end = '')
 else:
-  for x in stack[:-M]:
+  for x in stack:
     print(x, end = '')
