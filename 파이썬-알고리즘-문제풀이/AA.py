77fad46963fbdc1b9061dfe0db5ecaaaba3b9adc
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-28 13:20:55 +0900

    섹션5-6

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -1,10 +1,20 @@
 import sys
+from collections import deque
 
 sys.stdin=open("input.txt", "r")
 N, M = map(int, input().split())
-arr = [i+1 for i in range(N)]
-while len(arr) != 1:
-  for _ in range(M-1):
-    arr.append(arr.pop(0))
-  arr.pop(0)
-print(arr[0])
+arr = list(map(int, input().split()))
+arr = [[arr[i], i] for i in range(N)]
+arr = deque(arr)
+cnt = 0
+while True:
+  p = arr.popleft()
+  for i in range(len(arr)):
+    if p[0] < arr[i][0]:
+      arr.append(p)
+      break 
+  else:
+    cnt += 1
+    if p[1] == M:
+      print(cnt)
+      sys.exit()
--- 파이썬-알고리즘-문제풀이/섹션 5/6. 응급실/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 5/6. 응급실/AA.py
@@ -1,21 +1,20 @@
 import sys
 from collections import deque
-#sys.stdin=open("input.txt", "r")
 
+#sys.stdin=open("input.txt", "r")
 N, M = map(int, input().split())
 arr = list(map(int, input().split()))
-arr = deque([[arr[i], i] for i in range(N)])
+arr = [[arr[i], i] for i in range(N)]
+arr = deque(arr)
 cnt = 0
-
-maxx = -1
 while True:
   p = arr.popleft()
-  if any(p[0] < arr[i][0] for i in range(len(arr))):
-    arr.append(p)
+  for i in range(len(arr)):
+    if p[0] < arr[i][0]:
+      arr.append(p)
+      break 
   else:
     cnt += 1
     if p[1] == M:
       print(cnt)
-      break
-
-
+      sys.exit()
