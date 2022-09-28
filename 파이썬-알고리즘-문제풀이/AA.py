0868c098957ff190f569eb7e0c6118679b2da49e
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-29 00:21:59 +0900

    섹션6-11

--- 파이썬-알고리즘-문제풀이/AA.py
+++ 파이썬-알고리즘-문제풀이/AA.py
@@ -2,23 +2,19 @@ import sys
 import heapq as  hq
 sys.stdin=open("input.txt", "r")
 
-def D(L, s):
+def D(L, s, sum):
   global cnt
-  if L == M:
-    for x in res:
-      print(x, end = ' ')
-    print()
-    cnt += 1
-    return
+  if L == K:
+    if sum % M == 0 :
+      cnt += 1
   else:
     for i in range(s, N):
-      res[L] = i+1
-      D(L+1, i+1)
+      D(L+1, i+1, sum+arr[i])
 
     
-N, M = map(int, input().split())
-arr = [i+1 for i in range(N)]
-res = [0] * M
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+M = int(input())
 cnt = 0
-D(0, 0)
+D(0, 0, 0)
 print(cnt)
--- 파이썬-알고리즘-문제풀이/섹션 6/11. 수들의 조합/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 6/11. 수들의 조합/AA.py
@@ -1,11 +1,20 @@
 import sys
-import itertools as it
+import heapq as  hq
 #sys.stdin=open("input.txt", "r")
-n, k=map(int, input().split())
-a=list(map(int, input().split()))
-m=int(input())
-cnt=0
-for x in it.combinations(a, k):
-    if sum(x)%m==0:
-        cnt+=1
+
+def D(L, s, sum):
+  global cnt
+  if L == K:
+    if sum % M == 0 :
+      cnt += 1
+  else:
+    for i in range(s, N):
+      D(L+1, i+1, sum+arr[i])
+
+    
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
+M = int(input())
+cnt = 0
+D(0, 0, 0)
 print(cnt)
