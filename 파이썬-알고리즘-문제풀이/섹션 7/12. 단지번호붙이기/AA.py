57abb067e657174f5141c54c002d3009f3bd1fe6
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-09-29 17:20:06 +0900

    섹션6-11

--- 파이썬-알고리즘-문제풀이/섹션 7/12. 단지번호붙이기/AA.py
+++ 파이썬-알고리즘-문제풀이/섹션 7/12. 단지번호붙이기/AA.py
@@ -2,14 +2,16 @@ import sys
 from collections import deque
 #sys.stdin=open("input.txt", "r")
 
-def DFS(P, h):
+def DFS(P):
+  global cnt
   a, b = P
-  ch[a][b] = 1
   for i in range(4):
     x = a + dx[i]
     y = b + dy[i]
-    if 0 <= x < N and 0 <= y < N and mapp[x][y] > h and ch[x][y] == 0:
-      DFS((x, y), h)
+    if 0 <= x < N and 0 <= y < N and mapp[x][y] == 1:
+      mapp[x][y] = 0
+      cnt += 1
+      DFS((x, y))
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0 ,-1]
@@ -20,22 +22,12 @@ N = int(input())
 mapp = [list(map(int, input())) for _ in range(N)]
 Q = deque()
 res = []
-
 for i in range(N):
   for j in range(N):
     if mapp[i][j] == 1:
       cnt = 1
       mapp[i][j] = 0 
-      Q.append((i, j))
-      while Q:
-        a, b = Q.popleft()
-        for k in range(4):
-          x = a + dx[k]
-          y = b + dy[k]
-          if 0 <= x < N and 0 <= y < N and mapp[x][y] == 1:
-            mapp[x][y] = 0
-            Q.append((x, y))
-            cnt += 1
+      DFS((i, j))
       res.append(cnt)
       
 res.sort()
