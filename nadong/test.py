76babf8f4abe8d8baa84295452070e814ddfb13c
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-17 16:13:45 +0900

    복습

--- nadong/test.py
+++ nadong/test.py
@@ -1,24 +1,30 @@
 import sys
 sys.stdin=open("input.txt", "r")
-def count(x): # x로 이루어진 덩어리 갯수를 찾느다 .
-  p = -1 
-  cnt = 0
-  for i in range(len(s)):
-    if s[i] == x:
-        p = i  
-    else:
-      if p != -1:
-        cnt += 1
-        p = - 1
-  else:
-      if p != -1:
-        cnt += 1
+sys.setrecursionlimit(10**6)
 
-  return cnt
-s = list(map(int, input()))
-res1 = count(0)
-res2 = count(1)
-if res1 >= res2:
-    print(res2)
-else:
-    print(res1)
+
+def main():
+  n, k = map(int, input().split())
+  s = input()
+  
+  ch = [0] * n
+  cnt = 0
+  for i in range(n):
+    if s[i] == 'P':
+      for j in range(max(0, i-k), i):
+        if s[j] == 'H' and ch[j] == 0:
+          cnt +=1
+          ch[j] = 1
+          break 
+      else:
+        for j in range(min(n-1, i+1), i+k+1):
+          if j < n and s[j] == 'H' and ch[j] == 0:
+            cnt +=1
+            ch[j] = 1
+            break
+  print(cnt)
+       
+
+
+if __name__ == "__main__":
+  main()
