eab103349a4f5fb34f91651d9cb81e7d79c80715
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-18 15:36:41 +0900

    복습 txt 수정

--- nadong/test.py
+++ nadong/test.py
@@ -1,30 +1,28 @@
+
 import sys
 sys.stdin=open("input.txt", "r")
 sys.setrecursionlimit(10**6)
 
 
 def main():
-  n, k = map(int, input().split())
-  s = input()
-  
-  ch = [0] * n
-  cnt = 0
-  for i in range(n):
-    if s[i] == 'P':
-      for j in range(max(0, i-k), i):
-        if s[j] == 'H' and ch[j] == 0:
-          cnt +=1
-          ch[j] = 1
-          break 
-      else:
-        for j in range(min(n-1, i+1), i+k+1):
-          if j < n and s[j] == 'H' and ch[j] == 0:
-            cnt +=1
-            ch[j] = 1
-            break
-  print(cnt)
-       
+  n = int(input())
+  arr = []
+  for _ in range(n):
+    arr.append(int(input()))
+  res = 0
+  k = 1
+  while arr:
+    for i in range(len(arr)):
+      if arr[i] == k:
+        res += k 
+        k += 1
+        arr.pop(i)
+        break
+  print(res)
+      
+        
 
 
+   
 if __name__ == "__main__":
   main()
