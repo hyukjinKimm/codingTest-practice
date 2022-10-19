931eddd24edaac862abe214c0677b50cf09dab76
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-19 16:22:57 +0900

    복습내용 추가

--- nadong/test.py
+++ nadong/test.py
@@ -1,5 +1,3 @@
-
-
 import sys
 sys.stdin=open("input.txt", "r")
 sys.setrecursionlimit(10**6)
@@ -7,27 +5,17 @@ sys.setrecursionlimit(10**6)
 
 def main():
   n, m, k = map(int, input().split())
-  genre = [[] for _ in range(m)]
+  candidate = [0] * (n+1)
   for i in range(m):
-    genre[i].append(input().split())
-  res = 0 
-  s = set()
-  while k > 0:
-    maxx = 0
-    for i in range(m):
-      print(i)
-      if maxx < float(genre[i][1]):
-        if genre[i][0] not in s:
-          maxx = float(genre[i][1])
-          index = i
-    s.add(int(genre[i][0]))
-    res += maxx 
-    genre[i].pop(0)
-    genre[i].pop(0)
-
-    k -= 1
-    
-    
+    genre = input().split()
+    for i in range(0, 2*n, 2):
+      if candidate[int(genre[i])] < float(genre[i+1]):
+        candidate[int(genre[i])] = float(genre[i+1])
+  candidate.sort()
+  res = 0
+  for i in range(k):
+    res += candidate[n-i]
+  print(round(res, 1))
 
 
 
