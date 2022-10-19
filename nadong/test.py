ad540ad28463dffbeca6100790afa6b594724474
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-19 16:08:48 +0900

    복습

--- nadong/test.py
+++ nadong/test.py
@@ -1,28 +1,40 @@
 
+
 import sys
 sys.stdin=open("input.txt", "r")
 sys.setrecursionlimit(10**6)
 
 
 def main():
-  n = int(input())
-  arr = []
-  for _ in range(n):
-    arr.append(int(input()))
-  res = 0
-  k = 1
-  while arr:
-    for i in range(len(arr)):
-      if arr[i] == k:
-        res += k 
-        k += 1
-        arr.pop(i)
-        break
-  print(res)
-      
-        
+  n, m, k = map(int, input().split())
+  genre = [[] for _ in range(m)]
+  for i in range(m):
+    genre[i].append(input().split())
+  res = 0 
+  s = set()
+  while k > 0:
+    maxx = 0
+    for i in range(m):
+      print(i)
+      if maxx < float(genre[i][1]):
+        if genre[i][0] not in s:
+          maxx = float(genre[i][1])
+          index = i
+    s.add(int(genre[i][0]))
+    res += maxx 
+    genre[i].pop(0)
+    genre[i].pop(0)
+
+    k -= 1
+    
+    
 
 
-   
+
+
+
+  
+    
+
 if __name__ == "__main__":
   main()
