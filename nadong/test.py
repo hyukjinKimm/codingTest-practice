1844fad5cce09909ae2c520872a2471eb513d275
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-21 15:25:34 +0900

    get_bin함수의 중요성

--- nadong/test.py
+++ nadong/test.py
@@ -1,28 +1,33 @@
 import sys
+
 sys.stdin=open("input.txt", "r")
-sys.setrecursionlimit(10**6)
+
+def get_bin(x):
+  cnt = 0
+  sum = 1
+  while True:
+    sum = 2 ** cnt 
+    if sum >= x:
+      return sum
+    cnt += 1 
+
+  
 
 
 def main():
-  n, m, k = map(int, input().split())
-  candidate = [0] * (n+1)
-  for i in range(m):
-    genre = input().split()
-    for i in range(0, 2*n, 2):
-      if candidate[int(genre[i])] < float(genre[i+1]):
-        candidate[int(genre[i])] = float(genre[i+1])
-  candidate.sort()
-  res = 0
-  for i in range(k):
-    res += candidate[n-i]
-  print(round(res, 1))
-
-
-
-
+  n = int(input())
+  b = list(bin(n))
+  chk = 0
+  for i, c in enumerate(b[2:]):
+    if c == '1':
+      chk = i+1 
+  if chk == 1:
+    print(n, 0)
+  else:
+    print(get_bin(n), chk)
 
   
-    
+  
 
 if __name__ == "__main__":
   main()
