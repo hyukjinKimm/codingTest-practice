0315db639dc2ec69c360a9b80b7888d1b765180f
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-10-23 23:05:15 +0900

    복습수정

--- nadong/test.py
+++ nadong/test.py
@@ -1,42 +1,19 @@
 import sys
-
+import heapq as hq
 sys.stdin=open("input.txt", "r")
 
 
-def solve(n):
-  k  = -1
-  ki = 0
-  for i in range(len(n)-1, 0, -1):
-    for j in range(i-1, -1, -1):
-      if int(n[j]) < int(n[i]):
-        if j > k :
-          ki = i
-          k=j 
-        elif j == k:
-          if n[i] < n[ki] :
-            ki = i
-  else:
-    if k == -1:
-      print('BIGGEST')
-    else:
-      n[k], n[ki] = n[ki], n[k]
-      res = n[:k+1]
-      res += sorted(n[k+1:])
-      print("".join(res))
-    
-
-
-
 def main():
-  t = int(input())
-  for i in range(t):
-    n = list(input())
-    if len(n) == 1 or len(n) == 2:
-      print('BIGGEST')
-      return
-    solve(n)
-
-dd
+  n = int(input())
+  res = list(map(int, input().split()))
+  hq.heapify(res)
+  for i in range(n-1):
+    tmp = list(map(int, input().split()))
+    for j in range(n):
+      if tmp[j]  > res[0]:
+        hq.heappop(res)
+        hq.heappush(res, tmp[j])
+  print(res[0])
 
    
 if __name__ == "__main__":
