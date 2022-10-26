e7d956d50584077f61f02b1172c6c5d049246287
hyukjinkimm <hyukjin.kim.dev@gmail.com>
2022-10-26 16:35:08 +0900

    복습 업데이트

--- nadong/test.py
+++ nadong/test.py
@@ -1,36 +1,32 @@
 import sys
-import heapq as hq
+from collections import deque
 sys.stdin=open("input.txt", "r")
+
+n, m, v = map(int, input().split())
+graph = [[0] * (n+1) for _ in range(n+1)]
+
+for _ in range(m):
+  a, b = map(int, input().split())
+  graph[a][b] = 1
+
+ch = [0] * (n+1)
+
+def DFS(x):
+  print(x, end = ' ')
+  for i in range(n+1):
+    if graph[x][i] == 1 and ch[i] == 0:
+      ch[i] = 1 
+      DFS(i)
 
 
 def main():
-  n, m, k = map(int, input().split())
-  beer = []
-  for _ in range(k):
-    beer.append(list(map(int, input().split())))
-  beer.sort(key=lambda x:(x[1], -x[0] ))
-  print(beer)
-  eat = []
-  s = 0
-  for i in range(k):
-    now_alchol = beer[i][1]
-    hq.heappush(eat, beer[i][0])
-    s += beer[i][0]
-    if len(eat) == n:
-      if s < m:
-        s -= hq.heappop(eat)
-      else:
-        print(now_alchol)
-        return
-  else:
-    print(-1)
-
-        
-
+  ch[v] = 1
+  DFS(v)
   
-    
 
-  
-   
+  arr = deque()
+
+
+      
 if __name__ == "__main__":
   main()
