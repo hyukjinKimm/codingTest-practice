c6b29dff77033d302255dfcf6fd02f859e5bcf1e
hyukjinKimm <hyukjin.kim.dev@gmail.com>
2022-10-27 21:02:38 +0900

    복습

--- nadong/test.py
+++ nadong/test.py
@@ -1,36 +1,43 @@
-import sys
-import heapq as hq
-sys.stdin=open("input.txt", "r")
 
+import sys
+from collections import deque
+sys.stdin=open("C:\github\codingTest-practice\\nadong\input.txt", "r")
+
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def D(x):
+  global cnt, visited
+  visited[x] = 1
+  cnt += 1
+  for v in graph[x]:
+    if visited[v] == 0:
+      D(v)
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+res = -1
+cnt = 0
+visited = [0] * (n+1)
+for i in range(m):
+  a, b = map(int, input().split())
+  graph[b].append(a)
 
 def main():
-  n, m, k = map(int, input().split())
-  beer = []
-  for _ in range(k):
-    beer.append(list(map(int, input().split())))
-  beer.sort(key=lambda x:(x[1], -x[0] ))
-  print(beer)
-  eat = []
-  s = 0
-  for i in range(k):
-    now_alchol = beer[i][1]
-    hq.heappush(eat, beer[i][0])
-    s += beer[i][0]
-    if len(eat) == n:
-      if s < m:
-        s -= hq.heappop(eat)
-      else:
-        print(now_alchol)
-        return
-  else:
-    print(-1)
+  global cnt, res, visited
+  ch = [0] * (n+1)
+  for i in range(1, n+1):
+    cnt = 0
+    visited = [0] * (n+1)
+    D(i)
+    print(visited)
+    ch[i] = cnt
+    res = max(cnt, res)
 
-        
-
-  
-    
-
-  
-   
+  for i in range(1, n+1):
+    if ch[i] == res:
+      print(i, end = ' ')
 if __name__ == "__main__":
   main()
